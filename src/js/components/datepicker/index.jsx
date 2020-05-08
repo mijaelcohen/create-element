7d1ba88623abd2d0f58061586a81ffe951b29ba8
Mijail Cohen <mijail.cohen@despegar.com>
2020-05-08 16:40:33 -0300

    to hooks

--- src/js/components/datepicker/index.jsx
+++ src/js/components/datepicker/index.jsx
@@ -1,59 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { distpicker, datepicker } from './params';
 
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.mountDatepicker = this.mountDatepicker.bind(this);
-    this.mountDistribution = this.mountDistribution.bind(this);
-    this.datepickerContainer = React.createRef();
-    this.distpickerContainer = React.createRef();
-    this.state = {
-      showDist: false,
-      showDate: false,
-    }
-  }
-  componentDidMount(){    
+function Form () {
+  
+  const datepickerContainer = React.createRef();
+  const distpickerContainer = React.createRef();
+  
+  const [showDist, setShowDist] = useState(0);
+  const [showDate, setShowDate] = useState(0);
+  
+  useEffect(()=>{    
     datepicker.mount();
     distpicker.mount();
-  }
-  componentDidCatch(error, message){
-    console.error(error, message);
-  }
+  });
 
-  mountDatepicker() {
+  const mountDatepicker = () => {
     datepicker.dispatch({
-      type: this.state.showDate ? 'hide' : 'show', 
+      type: showDate ? 'hide' : 'show', 
       payload: {
-        showAt: this.datepickerContainer.current
+        showAt: datepickerContainer.current
       }
     })
-    this.setState({showDate: !this.state.showDate});
+    setShowDate(!showDate);
   }
-  mountDistribution(){
+  const mountDistribution = () =>{
     distpicker.dispatch({
-      type: this.state.showDist ? 'hide' : 'show',
+      type: showDist ? 'hide' : 'show',
       payload: {
-        showAt: this.distpickerContainer.current
+        showAt: distpickerContainer.current
       }
     })
-    this.setState({showDist: !this.state.showDist});
+    setShowDist(!showDist)
   }
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Prueba de componentes</h1>
-        <a ref={this.datepickerContainer} className="eva-3-btn-ghost -md -darken" onClick={this.mountDatepicker}>
-          <em className="btn-text">Date picker</em>
-        </a>
-        <a ref={this.distpickerContainer} className="eva-3-btn-ghost -md -darken" onClick={this.mountDistribution}>
-          <em className="btn-text">Distribution picker</em>
-        </a>
-      </React.Fragment>
-    );
-  }
+ 
+  return (
+    <React.Fragment>
+      <h1>Prueba de componentes</h1>
+      <a ref={datepickerContainer} className="eva-3-btn-ghost -md -darken" onClick={mountDatepicker}>
+        <em className="btn-text">Date picker</em>
+      </a>
+      <a ref={distpickerContainer} className="eva-3-btn-ghost -md -darken" onClick={mountDistribution}>
+        <em className="btn-text">Distribution picker</em>
+      </a>
+    </React.Fragment>
+  );
+  
 }
 
 const container = document.getElementById("container");
